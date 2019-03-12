f524530a20635749e49176db195e1b44faeb781d
Sergey Shliakhov <husband.sergey@gmail.com>
2019-03-12 11:31:07 +0200

    Add flush command feature

---
+++ src/Console/Commands/FlushCommand.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Matchish\ScoutElasticSearch\Console\Commands;
+
+use Illuminate\Console\Command;
+use Laravel\Scout\Searchable;
+use Matchish\ScoutElasticSearch\Searchable\SearchableList;
+use Matchish\ScoutElasticSearch\Searchable\SearchableListFactory;
+
+final class FlushCommand extends Command
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $signature = 'scout:flush {searchable? : The name of the searchable}';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $description = 'Flush the index of the the given searchable';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(SearchableListFactory $factory): void
+    {
+        $command = $this;
+        $searchables = (array) $command->argument('searchable');
+        $factory->make()->each(function ($searchable) {
+            /** @var Searchable $searchable */
+            $searchable::makeAllSearchable();
+            $searchable::removeAllFromSearch();
+            $this->output->success('All ['.$searchable.'] records have been flushed.');
+        });
+    }
+}
