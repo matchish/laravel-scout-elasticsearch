5905f24d4ef5acafc321d85ec78ad3b4454a50b2
Sergey Shliakhov <husband.sergey@gmail.com>
2019-03-13 15:11:11 +0200

    Fix TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ use Orchestra\Testbench\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    public function setUp()
+    public function setUp(): void
     {
         parent::setUp();
 
