bdd2b68e0c5a6f7e0999dea969bdf656b6b9ed0f
Sergey Shliakhov <husband.sergey@gmail.com>
2019-03-23 12:07:20 +0200

    Add states for product factory

--- tests/laravel/database/factories/ProductFactory.php
+++ tests/laravel/database/factories/ProductFactory.php
@@ -8,5 +8,59 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     return [
         'title' => $faker->text,
+        'custom_key' => $faker->uuid,
+        'price' => $faker->randomDigitNotNull
+    ];
+});
+
+$factory->state(Product::class, 'iphone', function (Faker $faker) {
+    return [
+        'title' => 'IPhone ' . $faker->randomDigitNotNull,
+        'price' => $faker->randomDigitNotNull
+    ];
+});
+
+$factory->state(Product::class, 'kindle', function (Faker $faker) {
+    return [
+        'title' => 'Amazon Kindle Fire ' . $faker->randomDigitNotNull,
+        'price' => $faker->randomDigitNotNull
+    ];
+});
+
+$factory->state(Product::class, 'promo', function (Faker $faker) {
+    return [
+        'price' => 100,
+    ];
+});
+
+
+$factory->state(Product::class, 'used', function (Faker $faker) {
+    return [
+        'type' => 'used',
+    ];
+});
+
+
+$factory->state(Product::class, 'like new', function (Faker $faker) {
+    return [
+        'type' => 'like new',
+    ];
+});
+
+$factory->state(Product::class, 'new', function (Faker $faker) {
+    return [
+        'type' => 'new',
+    ];
+});
+
+$factory->state(Product::class, 'cheap', function (Faker $faker) {
+    return [
+        'price' => $faker->numberBetween(30, 70),
+    ];
+});
+
+$factory->state(Product::class, 'luxury', function (Faker $faker) {
+    return [
+        'price' => $faker->numberBetween(1000),
     ];
 });
