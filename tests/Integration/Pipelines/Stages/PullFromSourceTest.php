5d04f57b5c19aafe046d3b3cde67357b720968f6
Sergey Shliakhov <husband.sergey@gmail.com>
2019-03-23 10:53:11 +0200

    Add test

--- tests/Integration/Pipelines/Stages/PullFromSourceTest.php
+++ tests/Integration/Pipelines/Stages/PullFromSourceTest.php
@@ -96,4 +96,37 @@ final class PullFromSourceTest extends IntegrationTestCase
         $response = $this->elasticsearch->search($params);
         $this->assertEquals($productsAmount, $response['hits']['total']);
     }
+
+    public function test_push_soft_delete_meta_data()
+    {
+        $this->app['config']['scout.soft_delete'] = true;
+
+        $dispatcher = Product::getEventDispatcher();
+        Product::unsetEventDispatcher();
+
+        $productsAmount = rand(1, 5);
+
+        factory(Product::class, $productsAmount)->create();
+
+        Product::setEventDispatcher($dispatcher);
+        $this->elasticsearch->indices()->create([
+            'index' => 'products_index',
+            'body' => ['aliases' => ['products' => new \stdClass()]]
+        ]);
+        $stage = new PullFromSource($this->elasticsearch);
+        $stage([Index::fromSearchable(new Product()), new Product()]);
+        $this->elasticsearch->indices()->refresh([
+            'index' => 'products',
+        ]);
+        $params = [
+            "index" => 'products',
+            "body" => [
+                "query" => [
+                    "match_all" => new \stdClass()
+                ]
+            ]
+        ];
+        $response = $this->elasticsearch->search($params);
+        $this->assertEquals(0, $response['hits']['hits'][0]['_source']['__soft_deleted']);
+    }
 }
