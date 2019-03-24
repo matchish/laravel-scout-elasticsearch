7d4b65404a2c2f3ec935d5955c2f2e95469c679d
Sergey Shliakhov <husband.sergey@gmail.com>
2019-03-24 06:41:14 +0200

    Add test
    Test delete with custom key

--- tests/Unit/ElasticSearch/Params/BulkTest.php
+++ tests/Unit/ElasticSearch/Params/BulkTest.php
@@ -21,6 +21,20 @@ class BulkTest extends TestCase
         ], $payload);
     }
 
+    public function test_delete_with_custom_key_name()
+    {
+        $this->app['config']['scout.key'] = 'title';
+        $bulk = new Bulk();
+        $product = new Product(['title' => 'Scout']);
+        $product->id = 2;
+        $bulk->delete($product);
+        $params = $bulk->toArray();
+
+        $this->assertEquals([
+            'body' => [['delete' => ['_index' => 'products', '_type' => '_doc', '_id' => 'Scout']]]
+        ], $params);
+    }
+
     public function testIndex()
     {
         $bulk = new Bulk();
@@ -34,6 +48,39 @@ class BulkTest extends TestCase
                 ['index' => ['_index' => 'products', '_type' => '_doc', '_id' => 2]],
                 ['title' => 'Scout', 'id' => 2],
             ]
-        ], $payload);
+        ], $params);
+    }
+
+    public function test_index_with_custom_key_name()
+    {
+        $this->app['config']['scout.key'] = 'title';
+        $bulk = new Bulk();
+        $product = new Product(['title' => 'Scout']);
+        $product->id = 2;
+        $bulk->index($product);
+        $params = $bulk->toArray();
+
+        $this->assertEquals([
+            'body' => [
+                ['index' => ['_index' => 'products', '_type' => '_doc', '_id' => 'Scout']],
+                ['title' => 'Scout', 'id' => 2],
+            ]
+        ], $params);
+    }
+
+    public function test_push_soft_delete_meta_data()
+    {
+        $this->app['config']['scout.soft_delete'] = true;
+        $bulk = new Bulk();
+        $product = new Product(['title' => 'Scout']);
+        $product->id = 2;
+        $bulk->index($product);
+        $params = $bulk->toArray();
+        $this->assertEquals([
+            'body' => [
+                ['index' => ['_index' => 'products', '_type' => '_doc', '_id' => 2]],
+                ['title' => 'Scout', '__soft_deleted' => 0, 'id' => 2],
+            ]
+        ], $params);
     }
 }
