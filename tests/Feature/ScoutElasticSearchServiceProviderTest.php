fb353108e6360ee6364873abb3a380a45c7701f3
Sergey Shliakhov <husband.sergey@gmail.com>
2019-04-13 08:48:10 +0300

    Remove useless assert

--- tests/Feature/ScoutElasticSearchServiceProviderTest.php
+++ tests/Feature/ScoutElasticSearchServiceProviderTest.php
@@ -2,9 +2,7 @@
 
 namespace Matchish\ScoutElasticSearch;
 
-use App\Product;
 use Tests\TestCase;
-use Matchish\ScoutElasticSearch\ElasticSearch\Index;
 
 class ScoutElasticSearchServiceProviderTest extends TestCase
 {
@@ -18,30 +16,7 @@ class ScoutElasticSearchServiceProviderTest extends TestCase
             '--tag' => 'config',
         ]);
 
-        app('config')->set('elasticsearch', require config_path('elasticsearch.php'));
-
         $this->assertFileExists(config_path('elasticsearch.php'));
 
-        $index = Index::fromSearchable(new Product());
-        $this->assertEquals([
-            'mappings' => [
-                '_doc' => [
-                    'properties' => [
-                        'created_at' => [
-                            'type' => 'date',
-                        ],
-                        'updated_at' => [
-                            'type' => 'date',
-                        ],
-                        'deleted_at' => [
-                            'type' => 'date',
-                        ],
-                    ],
-                ], ],
-            'settings' => [
-                'number_of_shards' => 1,
-                'number_of_replicas' => 0,
-            ],
-        ], $index->config());
     }
 }
