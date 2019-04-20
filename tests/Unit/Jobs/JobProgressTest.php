f929b7103e92f432376fdf48824f1d039dd4c2eb
Sergey Shliakhov <husband.sergey@gmail.com>
2019-04-20 11:20:42 +0300

    Finish job test progress

--- tests/Unit/Jobs/JobProgressTest.php
+++ tests/Unit/Jobs/JobProgressTest.php
@@ -3,8 +3,6 @@
 namespace Matchish\ScoutElasticSearch\Jobs;
 
 use Illuminate\Console\OutputStyle;
-use Matchish\ScoutElasticSearch\Console\Commands\DefaultProgressBar;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Tests\Fixtures\DummyOutput;
 use Tests\TestCase;
@@ -15,24 +13,34 @@ class JobProgressTest extends TestCase
     public function testJobProgress()
     {
         $events = app('events');
-        $jobId = uniqid();
         $job = new Job([
-            new ProgressReport(1, 'First', $jobId),
-            new ProgressReport(1, 'Second', $jobId),
-            new ProgressReport(3, 'Third', $jobId),
-            new ProgressReport(2, 'Fourth', $jobId),
-            new ProgressReport(1, 'Fitth', $jobId),
+            new ProgressReport(1, 'First step'),
+            new ProgressReport(2, 'Second step'),
+            new ProgressReport(1, 'Third'),
+            new ProgressReport(3, 'Next'),
+            new ProgressReport(1, 'Last'),
         ]);
         $output = new DummyOutput();
-        $outputStyle = new OutputStyle(new ArrayInput([]), new DummyOutput());
-        $progressBar = new DefaultProgressBar(new ProgressBar($outputStyle, $job->estimate()));
+        $outputStyle = new OutputStyle(new ArrayInput([]), $output);
+        $progressBar = $outputStyle->createProgressBar();
+        $progressBar->setMaxSteps($job->estimate() + 3);
+        $progressBar->setFormat('[%message%] %current%/%max%');
+        $progressBar->setMessage('Start');
         $sut = new JobProgress($progressBar, $events);
         $sut->start();
         while ($event = $job->next()) {
             $events->dispatch($event);
         }
         $sut->finish();
-        $this->assertEquals([], $output->getLogs());
+        $this->assertEquals([
+            'Start  0/11',
+            'First step  1/11',
+            'Second step  3/11',
+            'Third  4/11',
+            'Next  7/11',
+            'Last  8/11',
+            'Last 11/11',
+        ], $output->getLogs());
     }
 }
 
@@ -62,6 +70,6 @@ class Job
 
     public function next()
     {
-        return array_pop($this->stack);
+        return array_shift($this->stack);
     }
 }
