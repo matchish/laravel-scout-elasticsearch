cb15ae3325e26becd88f31f9f2b101f23bcda81b
Sergey Shliakhov <husband.sergey@gmail.com>
2019-05-06 06:39:12 +0300

    Create i18n files

---
+++ resources/lang/en/import.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'start' => 'Importing [:searchable]',
+    'done' => 'All [:searchable] records have been imported.',
+    'done.queue' => 'Import job dispatched to the queue. All [:searchable] will be availiable for search soon.',
+];
