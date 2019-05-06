a4f73e6a25ad68ccb4ce8fa7f3de9d2fb6ee2cf5
Sergey Shliakhov <husband.sergey@gmail.com>
2019-05-06 08:02:23 +0300

    Trim import done message

--- resources/lang/en/import.php
+++ resources/lang/en/import.php
@@ -2,5 +2,5 @@
 return [
     'start' => 'Importing [:searchable]',
     'done' => 'All [:searchable] records have been imported.',
-    'done.queue' => 'All [:searchable] will be availiable for search soon. (Dispatched to the queue)',
+    'done.queue' => 'Import job dispatched to the queue.',
 ];
