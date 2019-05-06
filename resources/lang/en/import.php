02c82bd40e767c85b7243a3289b337eae0ce03d2
Sergey Shliakhov <husband.sergey@gmail.com>
2019-05-06 07:48:39 +0300

    Change import done message

--- resources/lang/en/import.php
+++ resources/lang/en/import.php
@@ -2,5 +2,5 @@
 return [
     'start' => 'Importing [:searchable]',
     'done' => 'All [:searchable] records have been imported.',
-    'done.queue' => 'Import job dispatched to the queue. All [:searchable] will be availiable for search soon.',
+    'done.queue' => 'All [:searchable] will be availiable for search soon. (Dispatched to the queue)',
 ];
