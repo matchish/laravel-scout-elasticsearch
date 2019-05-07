9a80ce29950f1b2c1fcfebc5df7339f94239dfd5
Sergey Shliakhov <husband.sergey@gmail.com>
2019-05-07 07:46:26 +0300

    Add ticket model

---
+++ tests/laravel/app/Ticket.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Laravel\Scout\Searchable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ticket extends Model
+{
+    use Searchable, SoftDeletes;
+
+    protected $fillable = [
+        'from',
+        'to',
+        'date',
+    ];
+
+    public function getScoutKeyName()
+    {
+        return config('scout.key', $this->getKeyName());
+    }
+
+    public function getScoutKey()
+    {
+        return $this->getAttribute($this->getScoutKeyName());
+    }
+}
