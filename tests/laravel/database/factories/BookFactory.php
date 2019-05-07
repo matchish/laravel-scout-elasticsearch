9d317810dee803bc28fccc3a97affef3e95e86a2
Sergey Shliakhov <husband.sergey@gmail.com>
2019-05-07 07:57:34 +0300

    Add book factories

--- tests/laravel/database/factories/BookFactory.php
+++ tests/laravel/database/factories/BookFactory.php
@@ -8,7 +8,25 @@ use Faker\Generator as Faker;
 $factory->define(Book::class, function (Faker $faker) {
     return [
         'custom_key' => $faker->uuid,
-        'title' => $faker->text,
+        'title' => $faker->sentence,
+        'author' => $faker->name,
+        'year' => $faker->year,
+    ];
+});
+
+$factory->define(Book::class, 'new-york', function (Faker $faker) {
+    return [
+        'custom_key' => $faker->uuid,
+        'title' => "{$faker->sentence} New-York {$faker->sentence}",
+        'author' => $faker->name,
+        'year' => $faker->year,
+    ];
+});
+
+$factory->define(Book::class, 'barselona', function (Faker $faker) {
+    return [
+        'custom_key' => $faker->uuid,
+        'title' => "{$faker->sentence} Barselona {$faker->sentence}",
         'author' => $faker->name,
         'year' => $faker->year,
     ];
