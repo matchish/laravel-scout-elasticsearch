b3d84011b3008761170b97d60f0d2df4ebde35ca
Sergey Shliakhov <husband.sergey@gmail.com>
2019-05-09 12:00:17 +0300

    Test mixed search signature

---
+++ tests/Unit/MixedTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+
+use Laravel\Scout\Searchable;
+use Matchish\ScoutElasticSearch\Mixed;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+class MixedTest extends TestCase
+{
+
+    public function test_search_signature()
+    {
+        $searchable = new ReflectionClass(Searchable::class);
+        $searchableParameters = $searchable->getMethod('search')->getParameters();
+        $searchableDoc = $searchable->getMethod('search')->getDocComment();
+        $mixed = new ReflectionClass(Mixed::class);
+        $mixedParameters = $mixed->getMethod('search')->getParameters();
+        $mixedDoc = $mixed->getMethod('search')->getDocComment();
+
+        $this->assertEquals($searchableParameters, $mixedParameters);
+        $this->assertEquals($searchableDoc, $mixedDoc);
+    }
+}
