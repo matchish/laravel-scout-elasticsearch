e055cfb8b53307939126feacd9ff89ee7b316f24
Sergey Shliakhov <husband.sergey@gmail.com>
2019-05-09 12:02:23 +0300

    Implement mixed

---
+++ src/Mixed.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace Matchish\ScoutElasticSearch;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Builder;
+use Laravel\Scout\Searchable;
+
+final class Mixed
+{
+    /**
+     * Perform a search against the model's indexed data.
+     *
+     * @param  string  $query
+     * @param  \Closure  $callback
+     * @return \Laravel\Scout\Builder
+     */
+    public static function search($query = '', $callback = null)
+    {
+        return new Builder(new class extends Model{
+            use Searchable;
+        }, $query, $callback);
+    }
+}
