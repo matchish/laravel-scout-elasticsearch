0ed58efca053a9c39667d736f9b22b7ae0e09c46
Anne Datema <info@annedatema.nl>
2021-01-04 17:47:26 +0100

    Find searchable classes defined in higher order traits

--- tests/Unit/Searchable/SearchableListFactoryTest.php
+++ tests/Unit/Searchable/SearchableListFactoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Searchable;
 
+use App\Ticket;
 use Matchish\ScoutElasticSearch\Searchable\SearchableListFactory;
 use Tests\TestCase;
 
@@ -18,5 +19,18 @@ class SearchableListFactoryTest extends TestCase
 
         // There are 4 searchable models: Book, BookWithCustomKey, Product and Ticket
         $this->assertCount(4, $searchable);
+    }
+
+    public function test_find_searchable_trait_within_trait()
+    {
+        $this->assertFileExists(app()->path().'/Traits/Searchable.php');
+
+        // This should NOT throw "Error: Class 'Laravel\Telescope\TelescopeApplicationServiceProvider' not found"
+        $factory = new SearchableListFactory(app()->getNamespace(), app()->path());
+
+        $searchable = $factory->make();
+
+        // Model Ticket has trait that implements the Searchable trait.
+        $this->assertContains(Ticket::class, $searchable);
     }
 }
