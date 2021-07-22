4c21f5090123e63bed421e6ed06b8d89753d7111
Orestis Divintari <o.divintari@gmail.com>
2021-07-22 14:44:35 +0200

    Add whereIn clause filter
    The whereIn clause gives the ability to search a field against multiple values. This feature was added in scout 9.2.1

--- src/ElasticSearch/SearchFactory.php
+++ src/ElasticSearch/SearchFactory.php
@@ -6,6 +6,7 @@ use Laravel\Scout\Builder;
 use ONGR\ElasticsearchDSL\Query\Compound\BoolQuery;
 use ONGR\ElasticsearchDSL\Query\FullText\QueryStringQuery;
 use ONGR\ElasticsearchDSL\Query\TermLevel\TermQuery;
+use ONGR\ElasticsearchDSL\Query\TermLevel\TermsQuery;
 use ONGR\ElasticsearchDSL\Search;
 use ONGR\ElasticsearchDSL\Sort\FieldSort;
 
@@ -20,10 +21,17 @@ final class SearchFactory
     {
         $search = new Search();
         $query = new QueryStringQuery($builder->query);
-        if (! empty($builder->wheres)) {
+        if (static::hasWhereFilters($builder)) {
             $boolQuery = new BoolQuery();
-            foreach ($builder->wheres as $field => $value) {
-                $boolQuery->add(new TermQuery((string) $field, $value), BoolQuery::FILTER);
+            if (! empty($builder->wheres)) {
+                foreach ($builder->wheres as $field => $value) {
+                    $boolQuery->add(new TermQuery((string) $field, $value), BoolQuery::FILTER);
+                }
+            }
+            if (isset($builder->whereIns) && ! empty($builder->whereIns)) {
+                foreach ($builder->whereIns as $field => $arrayOfValues) {
+                    $boolQuery->add(new TermsQuery((string) $field, $arrayOfValues), BoolQuery::FILTER);
+                }
             }
             $boolQuery->add($query, BoolQuery::MUST);
             $search->addQuery($boolQuery);
@@ -43,5 +51,14 @@ final class SearchFactory
         }
 
         return $search;
+    }
+
+    /**
+     * @param Builder $builder
+     * @return bool
+     */
+    private static function hasWhereFilters($builder): bool
+    {
+        return (! empty($builder->wheres) || (isset($builder->whereIns) && ! empty($builder->whereIns)));
     }
 }
