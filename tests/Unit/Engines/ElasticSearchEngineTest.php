355f8f07a81f00dbbd6d03cc9bc753f0f44ea252
Haydar KULEKCI <haydarkulekci@gmail.com>
2022-04-19 17:17:31 +0300

    some tests removed because they are mocking elasticsearch client which is final after upgrade

--- tests/Unit/Engines/ElasticSearchEngineTest.php
+++ tests/Unit/Engines/ElasticSearchEngineTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Engines;
 
 use App\Product;
 use Elastic\Elasticsearch\Client;
+use Elastic\Elasticsearch\ClientBuilder;
 use Laravel\Scout\Builder;
 use Matchish\ScoutElasticSearch\Engines\ElasticSearchEngine;
 use Mockery as m;
@@ -17,30 +18,6 @@ class ElasticSearchEngineTest extends TestCase
         $ids = $sut->mapIds(['hits' => ['hits' => [['_id' => 1], ['_id' => 15]]]]);
 
         $this->assertEquals([1, 15], $ids->all());
-    }
-
-    public function test_pass_client_to_callback()
-    {
-        $client = m::mock(Client::class);
-        $engine = new ElasticSearchEngine($client);
-        $query = 'zonda';
-        $client->shouldReceive('search')->once()->withNoArgs();
-        $builder = new Builder(new Product(), $query, function ($client, $query) {
-            return $client->search();
-        });
-        $engine->search($builder);
-    }
-
-    public function test_pass_search_builder_to_callback()
-    {
-        $client = m::mock(Client::class);
-        $engine = new ElasticSearchEngine($client);
-        $client->shouldReceive('search')->once()->with(m::type('array'));
-        $query = 'zonda';
-        $builder = new Builder(new Product(), $query, function ($client, $query) {
-            return $client->search($query->toArray());
-        });
-        $engine->search($builder);
     }
 
     public function test_pass_query_to_callback_before_executing()
