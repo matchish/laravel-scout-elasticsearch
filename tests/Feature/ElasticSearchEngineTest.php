45c9f698fb08ae883bcaf38243410f6ffa0fc6aa
Haydar KULEKCI <haydarkulekci@gmail.com>
2022-04-20 11:40:19 +0300

    ElasticSearchEngine feature tests improved

---
+++ tests/Feature/ElasticSearchEngineTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use App\Product;
+use Illuminate\Support\Facades\Artisan;
+use Tests\IntegrationTestCase;
+
+final class ElasticSearchEngineTest extends IntegrationTestCase
+{
+    public function test_pass_empty_response(): void
+    {
+        $dispatcher = Product::getEventDispatcher();
+        Product::unsetEventDispatcher();
+
+        $productsAmount = random_int(1, 5);
+        $products = factory(Product::class, $productsAmount)->states(['iphone'])->create();
+        Product::setEventDispatcher($dispatcher);
+
+        Artisan::call('scout:import');
+
+        $results = Product::search('Quia', static function($client, $body) {
+            return $client->search(['index' => 'products', 'body' => $body->toArray()]);
+        })->raw();
+
+        $this->assertEmpty($results['hits']['hits']);
+    }
+
+    public function test_pass_with_response(): void
+    {
+        $dispatcher = Product::getEventDispatcher();
+        Product::unsetEventDispatcher();
+
+        $productsAmount = random_int(1, 5);
+        factory(Product::class, $productsAmount)->states(['iphone'])->create();
+        Product::setEventDispatcher($dispatcher);
+
+        Artisan::call('scout:import');
+
+        $results = Product::search('iphone', static function($client, $body) {
+            return $client->search(['index' => 'products', 'body' => $body->toArray()]);
+        })->raw();
+
+        $this->assertNotEmpty($results['hits']['hits']);
+    }
+}
