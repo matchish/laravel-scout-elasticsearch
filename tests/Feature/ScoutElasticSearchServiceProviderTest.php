defaf41f0354ceba2d91a95671eaaa60d803d454
Haydar KULEKCI <haydarkulekci@gmail.com>
2022-06-16 23:37:10 +0300

    test improved for provider

--- tests/Feature/ScoutElasticSearchServiceProviderTest.php
+++ tests/Feature/ScoutElasticSearchServiceProviderTest.php
@@ -34,4 +34,27 @@ class ScoutElasticSearchServiceProviderTest extends TestCase
         $provider = new ElasticSearchServiceProvider($this->app);
         $this->assertEquals([Client::class], $provider->provides());
     }
+
+    public function test_config_with_username()
+    {
+        $this->app['config']->set('elasticsearch.host', 'http://localhost:9200');
+        $this->app['config']->set('elasticsearch.user', 'elastic');
+        $this->app['config']->set('elasticsearch.password', 'pass');
+        $provider = new ElasticSearchServiceProvider($this->app);
+        $this->assertEquals([Client::class], $provider->provides());
+        /** @var Client $client */
+        $client = $this->app[Client::class];
+    }
+
+    public function test_config_with_cloud_id()
+    {
+        $this->app['config']->set('elasticsearch.cloud_id', 'Test:ZXUtY2VudHJhbC0xLmF3cy5jbG91ZC5lcy5pbyQ0ZGU0NmNlZDhkOGQ0NTk2OTZlNTQ0ZmU1ZjMyYjk5OSRlY2I0YTJlZmY0OTA0ZDliOTE5NzMzMmQwOWNjOTY5Ng==');
+        $this->app['config']->set('elasticsearch.api_key', '123456');
+        $provider = new ElasticSearchServiceProvider($this->app);
+        $this->assertEquals([Client::class], $provider->provides());
+        /** @var Client $client */
+        $client = $this->app[Client::class];
+        $this->assertEquals('ApiKey 123456', $client->getTransport()->getHeaders()['Authorization']);
+        $this->assertEquals('4de46ced8d8d459696e544fe5f32b999.eu-central-1.aws.cloud.es.io', $client->getTransport()->getNodePool()->nextNode()->getUri()->getHost());
+    }
 }
